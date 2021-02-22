5bc40b227337bd83378e667aee70d99907b17580
MichalPrzyl <michal.przyl@gmail.com>
2021-02-23 00:02:10 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="pl">
+<head>
+  <meta charset="utf-8">
+
+  <title>J0kerbaby Website</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+	<h1>Witaj</h1>
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
